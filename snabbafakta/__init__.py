7fd65041c891d8c28319c96639b5abc4b8b104d4
vienno <robin.linderborg@gmail.com>
2015-03-14 21:40:22 +0100

    added s3 bucket for images

--- snabbafakta/__init__.py
+++ snabbafakta/__init__.py
@@ -7,9 +7,10 @@ import os
 app = Flask(__name__)
 if os.environ.get('HEROKU_POSTGRESQL_COBALT_URL'):
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_COBALT_URL')
+	app.config['IMG_DIR'] = 'https://s3-eu-west-1.amazonaws.com/hackforsweden'
 else:	
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://snabbafakta:@localhost/snabbafakta'
-app.config['IMG_DIR'] = os.path.abspath('/Users/robinlinderborg/Desktop/img/')
+	app.config['IMG_DIR'] = os.path.abspath('/Users/robinlinderborg/Desktop/img/')
 
 db.init_app(app)
 migrate = Migrate(app, db)
