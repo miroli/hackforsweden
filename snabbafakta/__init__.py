86df6944f92f5a7242be72cc81d10003cc89701a
vienno <robin.linderborg@gmail.com>
2015-03-14 21:10:28 +0100

    heroku db

--- snabbafakta/__init__.py
+++ snabbafakta/__init__.py
@@ -5,7 +5,10 @@ from models import db
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://snabbafakta:@localhost/snabbafakta'
+if os.environ.get('HEROKU_POSTGRESQL_COBALT_URL'):
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_COBALT_URL')
+else:	
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://snabbafakta:@localhost/snabbafakta'
 app.config['IMG_DIR'] = os.path.abspath('/Users/robinlinderborg/Desktop/img/')
 
 db.init_app(app)
