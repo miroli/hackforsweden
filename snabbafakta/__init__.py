ff0e921df705cae0a454e086469779873533ea4d
vienno <robin.linderborg@gmail.com>
2015-03-14 21:49:48 +0100

    image s3

--- snabbafakta/__init__.py
+++ snabbafakta/__init__.py
@@ -5,12 +5,12 @@ from models import db
 import os
 
 app = Flask(__name__)
+app.config['IMG_DIR'] = 'https://s3-eu-west-1.amazonaws.com/hackforsweden'
 if os.environ.get('HEROKU_POSTGRESQL_COBALT_URL'):
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_COBALT_URL')
-	app.config['IMG_DIR'] = 'https://s3-eu-west-1.amazonaws.com/hackforsweden'
-else:	
+else:
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://snabbafakta:@localhost/snabbafakta'
-	app.config['IMG_DIR'] = os.path.abspath('/Users/robinlinderborg/Desktop/img/')
+	# app.config['IMG_DIR'] = os.path.abspath('/Users/robinlinderborg/Desktop/img/')
 
 db.init_app(app)
 migrate = Migrate(app, db)
